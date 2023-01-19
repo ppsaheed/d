6a99899c85133cebb065218e8e22246eb9443b49
ppsaheed <ppsaheed@gmail.com>
2023-01-20 00:16:20 +0530

    Update por.sh

--- por.sh
+++ por.sh
@@ -7,4 +7,5 @@ sudo apt install apparmor -y
 sudo apt install docker-compose -y
 sudo apt install cups –y
 docker-compose up
+#curl -fsSL https://tailscale.com/install.sh | sh
 sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest

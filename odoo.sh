84799a2531947955e874957e63005614c82ccd67
ppsaheed <ppsaheed@gmail.com>
2023-02-10 16:16:00 +0000

    Create odoo.sh

---
+++ odoo.sh
@@ -0,0 +1,22 @@
+sudo docker system prune -a --volumes
+sudo apt update
+sudo apt upgrade
+sudo apt install task-xfce-desktop blueman xfce4-terminal network-manager-gnome xfce4-power-manager -y
+sudo cp ./conf/lightdm/lightdm.conf /etc/lightdm/lightdm.conf -f
+sudo cp ./conf/lightdm/lightdm.conf /etc/lightdm/lightdm.conf -f
+sudo apt install docker.io -y
+sudo apt install apparmor -y
+sudo apt install docker-compose -y
+sudo apt install cups –y
+sudo apt install chromium -y
+docker-compose up
+sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+curl -fsSL https://tailscale.com/install.sh | sh
+
+#Hostname
+#cus_id
+#ssid_&_pass
+#legal acceptance
+
+#purge xfc panal
+#novnc_test

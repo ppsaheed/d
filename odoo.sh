df1f0d99351b2f1e669f965763718cd9b023ab5e
ppsaheed <ppsaheed@gmail.com>
2023-02-11 05:48:33 +0000

    Update odoo.sh

--- odoo.sh
+++ odoo.sh
@@ -9,7 +9,8 @@ sudo apt install apparmor -y
 sudo apt install docker-compose -y
 sudo apt install cups –y
 sudo apt install chromium -y
-docker-compose up
+#docker-compose up
+docker run -d --restart unless-stopped --name db -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=odoo -e POSTGRES_USER=odoo postgres:15 && docker run -d --restart unless-stopped --name web --link db -p 8069:8069 vuna2004/docker-odoo:16.0-arm64
 sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 curl -fsSL https://tailscale.com/install.sh | sh
 

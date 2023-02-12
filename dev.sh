03397b136e85c09f09416743c295c2b4c3c7d596
ppsaheed <ppsaheed@gmail.com>
2023-02-12 18:59:57 +0000

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,14 @@
+echo "alias i='sudo apt install '" >> ~/.bashrc
+echo "alias p='sudo apt purge '" >> ~/.bashrc
+echo "alias ss='sudo systemctl stop '" >> ~/.bashrc
+echo "alias sr='sudo systemctl restart '" >> ~/.bashrc
+echo "alias bt='sudo bluetoothctl scan on'" >> ~/.bashrc
+echo "alias s='sudo shutdown -h now '" >> ~/.bashrc
+echo "alias r='sudo reboot'" >> ~/.bashrc
+echo "alias e='sudo nano '" >> ~/.bashrc
+echo "alias up='cd ../..'" >> ~/.bashrc
+echo "alias c='clear'" >> ~/.bashrc
+echo "alias h='history'" >> ~/.bashrc
+echo "alias ..='cd ..'" >> ~/.bashrc
+echo "alias ...='cd ../..'" >> ~/.bashrc
+echo "alias g='grep'" >> ~/.bashrc
